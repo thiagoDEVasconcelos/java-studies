ec827c6a2d816d99e72582a8bbf2992febc6e407
Thiago Sousa <thiagovsousa21@gmail.com>
2026-05-03 15:55:43 -0300

    Operator Fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is ReelTrack);
+        System.out.println("This is ReelTrack");
         System.out.println("Movie: Top Gun: Maverick");
 
         int releaseYear = 2022;
